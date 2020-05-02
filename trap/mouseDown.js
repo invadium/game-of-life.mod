dc665b0c2be197af8349e2b9b988c5cd64b7e332
Igor Khotin <khotin@gmx.com>
2020-05-02 21:48:23 +0300

    [-] session clean up

--- trap/mouseDown.js
+++
@@ -1,7 +0,0 @@
-function mouseDown(e) {
-    let action = 1
-    if (e.buttons === 2) action = 2 
-    if (e.metaKey || e.altKey || e.shiftKey) action = 2
-
-    lab.grid.poke(e.clientX, e.clientY, action)
-}
