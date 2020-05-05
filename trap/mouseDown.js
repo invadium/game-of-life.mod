cfadf619b6f8f745a9204503f2d13de35e47d7f5
Igor Khotin <khotin@gmx.com>
2020-05-05 14:26:22 +0300

    [*] update mouse control to work with touchpads

--- trap/mouseDown.js
+++ trap/mouseDown.js
@@ -2,8 +2,13 @@ function mouseDown(e) {
 
     let action = 0
     switch(e.buttons) {
-        case 1: action = 1; break;
-        case 2: action = 2; break;
+        case 1:
+            if (e.metaKey || e.ctrlKey || e.altKey) action = 2
+            else action = 1
+            break
+        case 2:
+            action = 2
+            break
     }
 
     if (action) {
